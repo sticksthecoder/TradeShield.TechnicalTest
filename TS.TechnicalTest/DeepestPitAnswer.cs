626462e5e28b5da958f77ab60e7d099a4e4f92d2
Simphiwe Nkosi <sticksthedesigner@gmail.com>
2025-12-15 12:34:34 +0200

    feat: Partial implementation of pit detection and descent tracking

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -14,6 +14,17 @@ public class DeepestPitAnswer
                 // Find start of a decent
                 if (points[i] < points[i - 1] && points[i - 1] > 0)
                 {
+                    var leftPeak = points[i - 1];
+                    
+                    // To find the lowest point
+                    while (i < n && points[i] < points[i - 1])
+                    {
+                        i++;
+                    }
+                    
+                    // Becomes invalid when there's no movement or has reached an end
+                    if (i >= n)
+                        break;
                    
                 }
 
