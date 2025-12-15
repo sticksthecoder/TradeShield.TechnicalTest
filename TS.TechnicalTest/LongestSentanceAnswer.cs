90ad88185ec8a572178e53803d25279916b17afa
Simphiwe Nkosi <sticksthedesigner@gmail.com>
2025-12-15 11:56:13 +0200

    Initial implementation of longest sentence calculation

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,23 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        //separate string into sentences
+        var sentences = s.Split('.');
+        int highestNumber = 0;
+        
+        foreach (var sentence in sentences)
+        {
+            //separate sentence into words
+            var words = sentence.Trim().Split(' ');
+
+            if (words.Length > highestNumber)
+            {
+                highestNumber = words.Length;
+            }
+        }
+        
+        return highestNumber;
+        
+        
     }
 }
